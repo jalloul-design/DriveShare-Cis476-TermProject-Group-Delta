09988e391b43a57ae13bb3da86570e2cc81cdb8a
jalloul-design <jalloul@umich.edu>
2026-06-03 01:44:05 -0400

    Add SessionManager Singleton for tracking logged in user

--- auth/session_manager.py
+++ auth/session_manager.py
@@ -1,2 +1,31 @@
 # Owned By Mira
 # Singleton Class goes here, instances, methods like login, logout, get the current user
+
+class SessionManager:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SessionManager, cls).__new__(cls)
+            cls._instance.current_user_logged_in = None
+        return cls._instance
+
+    def login(self, user_email):
+        self.current_user_logged_in = user_email
+
+    def logout(self):
+        self.current_user_logged_in = None
+
+    def get_current_user(self):
+        return self.current_user_logged_in
+
+    def user_logged_in(self):
+        return self.current_user_logged_in is not None
+
+
+
+
+
+
+
+
