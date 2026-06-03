72325ca7c65f0f0e133f76af4650287667bbb447
jalloul-design <jalloul@umich.edu>
2026-06-03 01:14:11 -0400

    Adding Flask Secret Key Function and Register Blueprint with authenticy blueprint

---
+++ auth/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from database import init_db
+from auth.routes import auth_bp
+
+app = Flask(__name__)
+app.secret_key = ''
+app.register_blueprint(auth_bp)
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
