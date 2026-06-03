3d6b1d6a159fbac3e62fa65820fe9364cd9b8454
jalloul-design <jalloul@umich.edu>
2026-06-03 01:25:44 -0400

    Creating Login File to add login and logout functions

--- auth/login.py
+++ auth/login.py
@@ -1,2 +1,20 @@
 # Owned By Mira
-# Function that will look up user by email, vertify the password, and call session manager login
+# Function that will look up user by email, verification, and call session manager login
+
+from models.user import User
+from auth.session_manager import SessionManager
+
+def login(user_email, user_password):
+    user = User.verify_login(user_email, user_password)
+    if user is None:
+        return False, "This account does not exist, Invalid email or password"
+
+    session_manager = SessionManager()
+    session_manager.login(user['user_email'])
+    return True, 'Login Successful'
+
+def logout():
+    session_manager = SessionManager()
+    session_manager.logout()
+
+
